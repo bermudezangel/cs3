ac410ecb6a8ec385dc55c873e6a27bfad733ea80
bermudezangel <angelbermudez081206@gmail.com>
2024-03-20 12:54:07 -0500

    updat
    asdsadsadasd

--- u09a_graphTheory/labs/1_graphs_Connections/Graph.java
+++ u09a_graphTheory/labs/1_graphs_Connections/Graph.java
@@ -11,10 +11,16 @@ public class Graph
 {
    //Choose:
 	private Map<String, String> map;
-   private Map<String, List<String>> map;
+   //private Map<String, List<String>> map;
    
 	public Graph(String line)
 	{
+      map = new TreeMap<String, String>();
+      for (String s : line.split(" ")){
+         map.put(s.substring(0, 1), s.substring(1));
+         map.put(s.substring(1), s.substring(0, 1));
+         
+         
 	}
 
 	public boolean contains(String letter)

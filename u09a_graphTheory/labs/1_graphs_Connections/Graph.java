76530f706b5ba1987e1b404ea278e6ab3dd884b9
bermudezangel <angelbermudez081206@gmail.com>
2024-03-26 12:41:07 -0500

    updat

--- u09a_graphTheory/labs/1_graphs_Connections/Graph.java
+++ u09a_graphTheory/labs/1_graphs_Connections/Graph.java
@@ -10,30 +10,48 @@ import java.util.TreeMap;
 public class Graph
 {
    //Choose:
-	private Map<String, String> map;
+	private Map<String, Set<String>> map;
    //private Map<String, List<String>> map;
    
 	public Graph(String line)
 	{
-      map = new TreeMap<String, String>();
+      String silly = "";
+      String silly2 = "";
+      map = new TreeMap<String, Set<String>>();
       for (String s : line.split(" ")){
-         map.put(s.substring(0, 1), s.substring(1));
-         map.put(s.substring(1), s.substring(0, 1));
-         
-         
+         silly = s.substring(0, 1);
+         silly2 = s.substring(1);
+         if (map.get(silly) == null){
+            map.put(silly, new HashSet<String>());
+         }
+         if (map.get(silly2) == null){
+            map.put(silly2, new HashSet<String>());   
+         }
+         map.get(silly).add(silly2);
+         map.get(silly2).add(silly);
+      }
 	}
 
 	public boolean contains(String letter)
 	{
-	   return true;
+      if (
 	}
 
    public boolean connected(String src, String dest)
 	{
+   String place = "";
+    if (map.containsKey(){
+      return true;
+    }else{
+      for (String s : map.get(src){
+          place += s + ", ";
+          connected(s, 
+         
 	}
 
    private boolean connected(String src, String dest, String placesUsed)
    { 
+      return false;
    }
    
    @Override

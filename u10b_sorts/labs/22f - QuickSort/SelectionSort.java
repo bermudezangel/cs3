676b2cba65a35e6fc7416d1adfc10ffdb0ca69e7
bermudezangel <angelbermudez081206@gmail.com>
2024-05-03 12:41:45 -0500

    updat

---
+++ u10b_sorts/labs/22f - QuickSort/SelectionSort.java
@@ -0,0 +1,8 @@
+import static java.lang.System.*;
+import java.util.Arrays;
+import java.util.Collections.*;
+import java.util.ArrayList;
+
+public class SelectionSort
+{
+}
---
+++ u10b_sorts/labs/SelectionSort.java
@@ -0,0 +1,23 @@
+import static java.lang.System.*;
+import java.util.Arrays;
+import java.util.Collections.*;
+import java.util.ArrayList;
+
+public class SelectionSort
+{
+   private String[] sorty;
+   
+   public SelectionSort(String changeToArray){
+      sorty = changeToArray.split(" ");
+   }
+   
+   public String currState(){
+      return sorty.toString();
+   }
+   
+   public void sort(){
+   }
+   
+   public void sort(String[] temp){
+   }
+}
